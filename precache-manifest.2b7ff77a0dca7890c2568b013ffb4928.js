f5fb991a6f1051abdd1fe189766962207dd9153d
CalebRose <ctrose17@gmail.com>
2019-07-12 07:03:51 -0500

    Updates

---
+++ precache-manifest.2b7ff77a0dca7890c2568b013ffb4928.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d0614c5a3ba0b5f45e3d2e0e76c962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3c1135ab7a7ab2cad2",
+    "url": "/static/css/main.00b86d0f.chunk.css"
+  },
+  {
+    "revision": "4fe92bf4932204d6059f",
+    "url": "/static/js/2.04a8c83d.chunk.js"
+  },
+  {
+    "revision": "3a3c1135ab7a7ab2cad2",
+    "url": "/static/js/main.e4385e5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce0272ed48a4c133bc9a16545b0c6c74",
+    "url": "/static/media/Neon.ce0272ed.ttf"
+  },
+  {
+    "revision": "a80a5aa66aa9425f797d35f8470239f7",
+    "url": "/static/media/Outrun.a80a5aa6.otf"
+  },
+  {
+    "revision": "c86f6e311c62f1ee74affa41d9c3686f",
+    "url": "/static/media/Streamster.c86f6e31.ttf"
+  },
+  {
+    "revision": "e51b1da6b978a3c7ff09f603999ca50b",
+    "url": "/static/media/barcade.e51b1da6.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.2b7ff77a0dca7890c2568b013ffb4928.js"
 );
 
 self.addEventListener('message', (event) => {
